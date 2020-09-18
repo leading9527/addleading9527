4acdd40dcf107338c4242f1375c25ed406326771
“ <leading9527@163.com>
2020-09-19 02:32:08 +0800

    ““测试

---
+++ flex布局/js.js
@@ -0,0 +1,47 @@
+window.onload=function(){
+//    // var a=1
+//   // function arr(){
+//   //   console.log(this.a)
+//   //   setTimeout(()=>{
+//   //     console.log(this.a)
+//   //   },0)
+//   //   console.log(this.a)
+//   // }
+//   // arr()
+//   var arr='13111564'
+//   var a=''
+//   var b=arr.split('')
+//   console.log(b)
+//   for(var i=0;i<arr.length;i++){
+//     if(a.indexOf(b[i])==-1){
+//       a+=b[i]
+//     }
+//   }
+//   console.log(a,typeof(a))
+//   function f(item,i){
+//     setTimeout(() => {
+//       return f()
+//     }, 0);
+//     // for(var i=0;i<5;i++){
+//     //   console.log(i)
+//     //   return i
+//     // }
+//     // return 123
+//   }
+//   console.log(f())
+
+}
+
+  // var $get = function(URL,DATA={}){
+  //   return new Promise((reslove,reject)=>{
+  //     axios.get(URL,DATA).then(res=>{
+  //       reslove(res) 
+  //     })
+  //   });
+  // };
+  // $get(url,{}).then(res=>{
+  //   console.log(res)
+  // }).catch((res)=>{
+  //   console.log("catch:"+res)
+  // });
+  
